1d200b00118719659439e239bdd5b2aa396a6208
Jonathan Khoo <jkhoo98@gmail.com>
2020-01-23 02:45:18 +1300

    Only build V8 if not in Travis CI

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -179,46 +179,48 @@ echo "$chroot_cmd update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 
 chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 75
 # gcc 9 done
 
-# JavaScript V8 engine compile and install
-echo "Preparing to compile and install the JavaScript V8 engine."
-echo
-echo "If this fails, visit https://v8.dev/docs/build and follow the instructions."
-echo "This may take a while ..."
-echo
-set -x
+[ -z "$TRAVIS" ] && { # if not in Travis-CI
+  # JavaScript V8 engine compile and install
+  echo "Preparing to compile and install the JavaScript V8 engine."
+  echo
+  echo "If this fails, visit https://v8.dev/docs/build and follow the instructions."
+  echo "This may take a while ..."
+  echo
+  set -x
 
-: Making temporary directory
-mkdir ~/v8-build
-cd ~/v8-build
+  : Making temporary directory
+  mkdir ~/v8-build
+  cd ~/v8-build
 
-: Installing depot_tools
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="$PATH:$PWD/depot_tools"
-gclient
+  : Installing depot_tools
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  export PATH="$PATH:$PWD/depot_tools"
+  gclient
 
-: Getting V8 source code
-mkdir v8
-cd v8
-fetch v8
-cd v8
+  : Getting V8 source code
+  mkdir v8
+  cd v8
+  fetch v8
+  cd v8
 
-: Installing additional build dependencies
-gclient sync
-./build/install-build-deps.sh
+  : Installing additional build dependencies
+  gclient sync
+  ./build/install-build-deps.sh
 
-: Compiling V8
-./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
-ninja -C out.gn/x64.release d8
+  : Compiling V8
+  ./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
+  ninja -C out.gn/x64.release d8
 
-: Copying executable into place
-cp ./out.gn/x64.release/d8 "$ISOLATE_ROOT"/usr/bin/d8
+  : Copying executable into place
+  cp ./out.gn/x64.release/d8 "$ISOLATE_ROOT"/usr/bin/d8
 
-: Cleaning up
-cd ~
-rm -r v8-build
+  : Cleaning up
+  cd ~
+  rm -r v8-build
 
-set +x
-echo "JavaScript V8 installed into $ISOLATE_ROOT/usr/bin/d8"
-echo
+  set +x
+  echo "JavaScript V8 installed into $ISOLATE_ROOT/usr/bin/d8"
+  echo
+}
 
 echo 'Finished chroot installs!'
