5c7e858baea7f5fc08dea317136f4beaee188e54
Jonathan Khoo <jkhoo98@gmail.com>
2020-01-23 17:14:59 +1300

    Build V8 inside chroot

--- script/install/debootstrap.bash
+++ script/install/debootstrap.bash
@@ -181,46 +181,7 @@ chroot "$ISOLATE_ROOT" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g
 
 [ -z "$TRAVIS" ] && bash script/confirm.bash 'Install JavaScript V8 (submissions in V8 will fail without this!)' && {
   # JavaScript V8 engine compile and install
-  echo "Preparing to compile and install the JavaScript V8 engine."
-  echo
-  echo "If this fails, visit https://v8.dev/docs/build and follow the instructions."
-  echo "This may take a while ..."
-  echo
-  set -x
-
-  : Making temporary directory
-  mkdir ~/v8-build
-  cd ~/v8-build
-
-  : Installing depot_tools
-  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-  export PATH="$PATH:$PWD/depot_tools"
-  gclient
-
-  : Getting V8 source code
-  mkdir v8
-  cd v8
-  fetch v8
-  cd v8
-
-  : Installing additional build dependencies
-  gclient sync
-  ./build/install-build-deps.sh
-
-  : Compiling V8
-  ./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
-  ninja -C out.gn/x64.release d8
-
-  : Copying executable into place
-  cp ./out.gn/x64.release/d8 "$ISOLATE_ROOT"/usr/bin/d8
-
-  : Cleaning up
-  cd ~
-  rm -r v8-build
-
-  set +x
-  echo "JavaScript V8 installed into $ISOLATE_ROOT/usr/bin/d8"
-  echo
+  chroot "$ISOLATE_ROOT" bash < script/install/install-v8.bash
 }
 
 echo 'Finished chroot installs!'
---
+++ script/install/install-v8.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+echo "Preparing to compile and install the JavaScript V8 engine."
+echo
+echo "If this fails, visit https://v8.dev/docs/build and follow the instructions."
+echo "This may take a while ..."
+echo
+set -x
+
+: Installing Git
+apt-get install git
+
+
+: Making temporary directory
+mkdir /v8-build
+cd /v8-build
+
+: Installing depot_tools
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$PATH:$PWD/depot_tools"
+gclient --version
+
+: Getting V8 source code
+mkdir v8
+cd v8
+fetch v8
+cd v8
+
+: Installing additional build dependencies
+gclient sync
+./build/install-build-deps.sh
+
+: Compiling V8
+./tools/dev/v8gen.py x64.release -- v8_use_external_startup_data=false
+ninja -C out.gn/x64.release d8
+
+: Copying executable into place
+cp ./out.gn/x64.release/d8 /usr/bin/d8
+
+: Cleaning up
+cd /
+rm -rf v8-build
+
+set +x
+echo "JavaScript V8 installed into $ISOLATE_ROOT/usr/bin/d8"
+echo
